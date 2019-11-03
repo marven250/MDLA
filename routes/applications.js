f8eb50813f9b523d63bf54ef5bb35e20e9f704a6
Marven Mathelier <marven250@hotmail.com>
2019-11-03 18:10:11 -0500

    doneish

--- routes/applications.js
+++ routes/applications.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   let allTheApps = []
-  for (let i = 1; i <= 50; i++) {
+  for (let i = 1; i <= 100; i++) {
     if (i < 10) allTheApps.push({ name: `my-app-00${i}`, id: `${i}` })
     if (i >= 10) allTheApps.push({ name: `my-app-0${i}`, id: `${i}` })
   }
