c89d5e55fe424fc139b2a5dd0a2309b5b8fe9c01
Marven Mathelier <marven250@hotmail.com>
2019-11-03 07:41:50 -0500

    progresso

---
+++ routes/applications.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let allTheApps = []
+  for (let i = 1; i <= 50; i++) {
+    if (i < 10) allTheApps.push({ name: `my-app-00${i}`, id: `${i}` })
+    if (i >= 10) allTheApps.push({ name: `my-app-0${i}`, id: `${i}` })
+  }
+  res.json({
+    data: allTheApps
+  });
+});
+
+module.exports = router;
